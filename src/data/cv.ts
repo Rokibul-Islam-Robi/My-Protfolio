1be13dd18d09f479c27838c58911f3e6f78ca705
Rokibul Islam Robi <149813484+Rokibul-Islam-Robi@users.noreply.github.com>
2026-04-07 05:02:28 +0600

    Update CV/Resume link in navigation

--- src/data/cv.ts
+++ src/data/cv.ts
@@ -1,6 +1,6 @@
 export const cvLinks = {
-  cv: 'https://docs.google.com/document/d/1QNweciisbMwvnR1UETASAApQI84ZIvW12p7awR9TLYQ/edit?usp=sharing',
-  resume: 'https://drive.google.com/file/d/10sQpJainP8WreXqxFxaBebpV-Csfi0Rj/view?usp=drive_link',
+  cv: 'https://drive.google.com/drive/folders/1q5g-9U3152Srr5gYoFeO_1AErDK1pl2s',
+  resume: 'https://drive.google.com/drive/folders/1q5g-9U3152Srr5gYoFeO_1AErDK1pl2s',
   linkedin: 'https://www.linkedin.com/in/rokibulislam088',
   github: 'https://github.com/Rokibul-Islam-Robi'
-}; 
+};
