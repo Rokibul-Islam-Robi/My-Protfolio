fb7a3188d582e00c9881dc8be22e24aac36b8cbe
Rokibul Islam Robi <149813484+Rokibul-Islam-Robi@users.noreply.github.com>
2026-04-07 05:06:39 +0600

    Correct CV/Resume link to direct file

--- src/data/cv.ts
+++ src/data/cv.ts
@@ -1,6 +1,6 @@
 export const cvLinks = {
-  cv: 'https://drive.google.com/drive/folders/1q5g-9U3152Srr5gYoFeO_1AErDK1pl2s',
-  resume: 'https://drive.google.com/drive/folders/1q5g-9U3152Srr5gYoFeO_1AErDK1pl2s',
+  cv: 'https://drive.google.com/file/d/14Uh808pLkwEdmFjCWS0VRsoTmCjbObdp/view?usp=sharing',
+  resume: 'https://drive.google.com/file/d/14Uh808pLkwEdmFjCWS0VRsoTmCjbObdp/view?usp=sharing',
   linkedin: 'https://www.linkedin.com/in/rokibulislam088',
   github: 'https://github.com/Rokibul-Islam-Robi'
 };
